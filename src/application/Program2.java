a4a40e1af56ebdcc678ddad6bebd1c265bddd2bd
Luisa Araujo <luisa.araujo@ibm.com>
2022-11-17 21:17:26 -0300

    first commit

---
+++ src/application/Program2.java
@@ -0,0 +1,53 @@
+package application;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program2 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for( int i=1; i<=n; i++){
+            System.out.println("Employee #" + i +" data:");
+            System.out.print("Outsourced (y/n)? ");
+            char ch = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            if (ch == 'y'){
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                Employee emp = new OutsourcedEmployee(name, hours,valuePerHour, additionalCharge );
+                list.add(emp);
+            }
+            else {
+                Employee emp = new Employee(name, hours, valuePerHour);
+                list.add(emp);
+            }
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS:");
+        for (Employee emp : list){
+            System.out.println(emp.getName() + " - $ " + String.format("%.2f", emp.payment()));
+        }
+        sc.close();
+    }
+}
